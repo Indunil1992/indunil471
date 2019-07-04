48a9c6331c3ff009ecbc7bbee406972eaba55c52
Indunil1992 <indunil@adroitlogic.com>
2019-07-04 14:39:41 +0530

    123

--- indunil471/lambda.js
+++ indunil471/lambda.js
@@ -3,27 +3,42 @@ const sns = new AWS.SNS();
 
 exports.handler = function (event, context, callback) {
 
+    sns.publish({
+        Message: 'test run',
+        Subject: 'check SNS',
+        MessageAttributes: {},
+        MessageStructure: 'String',
+        TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestSNS'
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
+
+
 
     sns.publish({
         Message: 'hi this is your verification msg ',
         MessageAttributes: {
             'AWS.SNS.SMS.SMSType': {
                 'DataType': 'String',
-                'StringValue': 'Promotional'
+                'StringValue': 'Transactional'
             }
         },
-        PhoneNumber: '+94713245242'
+        PhoneNumber: '+9471'
     }).promise()
         .then(data => {
             // your code goes here
-            console.log(data);
+            console.log("success" + { data });
             callback(null, "Successfully executed to enduser data" + { data });
 
             // successful response
 
         })
         .catch(err => {
-            console.log(err);
+            console.log("error " + { err });
             // error handling goes here
             callback(null, "Successfully executed to enduser catch" + { err });
 
