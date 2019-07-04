41e2b93b48e5f136175eaa9d6f055047fccdd332
Indunil1992 <indunil@adroitlogic.com>
2019-07-04 13:25:30 +0530

    i1

---
+++ indunil471/lambda.js
@@ -0,0 +1,31 @@
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+
+exports.handler = function (event, context, callback) {
+
+
+    sns.publish({
+        Message: 'hi this is your verification msg ',
+        MessageAttributes: {
+            'AWS.SNS.SMS.SMSType': {
+                'DataType': 'String',
+                'StringValue': 'Transactional'
+            },
+            'AWS.SNS.SMS.SenderID': {
+                'DataType': 'String',
+                'StringValue': 'from Sigma'
+            }
+        },
+        PhoneNumber: '+94713245242'
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
+
+
+
+    callback(null, { "message": "Successfully executed" });
+}
